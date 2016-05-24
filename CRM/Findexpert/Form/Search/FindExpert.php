8a16b21f99b69218644f650c89e7b445cdf3d1c8
Erik Hommel <hommel@ee-atwork.nl>
2016-05-24 15:13:13 +0200

    sort sector list

--- CRM/Findexpert/Form/Search/FindExpert.php
+++ CRM/Findexpert/Form/Search/FindExpert.php
@@ -158,7 +158,7 @@ class CRM_Findexpert_Form_Search_FindExpert extends CRM_Contact_Form_Search_Cust
     foreach ($sectors['values'] as $sectorId => $sector) {
       $result[$sectorId] = $sector['label'];
     }
-    return $result;
+    return asort($result);
   }
 
   /**
